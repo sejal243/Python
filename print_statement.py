b0daf7dfb1d43eb0d67003154875979828dd4dc1
sejal <84629537+sejal243@users.noreply.github.com>
2024-02-26 20:54:31 +0530

    Add files via upload

---
+++ print_statement.py
@@ -0,0 +1,10 @@
+print(
+    "Hello World", end=" "
+)  # end means 'Hello World Good Bye We are learning Python' will be printed in one line
+print("Good Bye", end=" ")
+print("We are learning python")
+
+print("Hello World", end="Joker")
+# means 'Hello WorldJokerGood Bye We are learning Python' will be printed so basically that space b/w " " will create space else that word entered
+
+# print("a\"\\\\\'xyz\'\"\\")
